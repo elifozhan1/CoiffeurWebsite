25733bfec1616cb8da0a5ce67d290daaf1d694c8
Zeynepk50 <kebapcizeynep37@gmail.com>
2024-12-25 19:01:44 +0300

    front

--- CoiffeurWebsite/obj/Debug/net8.0/CoiffeurWebsite.AssemblyInfo.cs
+++ CoiffeurWebsite/obj/Debug/net8.0/CoiffeurWebsite.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoiffeurWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9adbdbcb1a3c79533df2a311de37465a2f594a1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c47217ad3ae634b8d773a4c1b46c410503ab3ab4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoiffeurWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoiffeurWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
