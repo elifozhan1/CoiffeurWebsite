8e4f04819cbf4959d8cabec55721d96602fbbd75
Elif Özhan <elif.ozhan1@ogr.sakarya.edu.tr>
2024-12-26 20:26:32 +0300

    RemoveViewModel

--- CoiffeurWebsite/obj/Debug/net8.0/CoiffeurWebsite.AssemblyInfo.cs
+++ CoiffeurWebsite/obj/Debug/net8.0/CoiffeurWebsite.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoiffeurWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25733bfec1616cb8da0a5ce67d290daaf1d694c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9ea88f23a41b7896d2bc51139022a0096713990")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoiffeurWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoiffeurWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
